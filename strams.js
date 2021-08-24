1f2afdcf9df424eb444a3e946e89dd037246e25b
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-08-24 10:37:28 +0300

    streams

---
+++ strams.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const server = require("http").createServer();
+
+server.on("request", (req, res) => {
+  //Solution 1 - will not work well because it stores a big data file into a variable.
+  //   fs.readFile("test-file.txt", (err, data) => {
+  //     if (err) console.log(err);
+  //     res.end(data);
+  //   });
+  //
+  //Solution 2: Streams
+  const readable = fs.createReadStream("test-file.txt");
+  readable.on("data", (chunk) => {
+    res.write(chunk);
+  });
+
+  readable.on("end", () => {
+    res.end();
+  });
+
+  readable.on("error", (err) => {
+    console.log(err);
+    res.statusCode = 500;
+    res.end("file not found");
+  });
+});
+
+server.listen(8000, "localhost", () => {
+  console.log("Listening on port 8000");
+});
